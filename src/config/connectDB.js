673985a607e7ede26d40c6e69725acba2a520e09
Iprito <hungvuphi2004@gmail.com>
2025-03-20 15:18:07 +0700

    second commit

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -4,6 +4,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('iprito', 'root', 'Iprito17', {
     host: 'localhost',
     dialect: 'mysql',
+    logging: false
 });
 
 // Hàm kết nối database
