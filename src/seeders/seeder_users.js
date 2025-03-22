9654f5fc7d445037bd0cc9c277ff6edb2c3f9422
Iprito <hungvuphi2004@gmail.com>
2025-03-22 13:07:25 +0700

    fourth commit

--- src/seeders/seeder_users.js
+++ src/seeders/seeder_users.js
@@ -5,9 +5,15 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Users', [
       {
+        email: 'admin@gmail.com',
+        password: 'Iprito17',
         firstName: 'John',
-        lastName: 'Doe',
-        email: 'example@example.com',
+        lastName: 'Michale',
+        address: '217 Dang Thuy Tram Street',
+        gender: 1,
+        typeRole: 'ROLE',
+        keyRole: 'R1',
+
         createdAt: new Date(),
         updatedAt: new Date(),
       },
